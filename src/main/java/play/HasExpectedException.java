3c78cc31a0f5db9e377fe87de5bcd930fcef13df
aristotle0x01 <aristotle0x01@proton.me>
2019-08-24 11:06:53 +0800

    minor

---
+++ src/main/java/play/HasExpectedException.java
@@ -0,0 +1,31 @@
+package play;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.core.StringStartsWith.startsWith;
+
+public class HasExpectedException {
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void throwsNothing() {
+
+    }
+
+    @Test
+    public void throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        throw new NullPointerException();
+    }
+
+    @Test
+    public void throwsNullPointerExceptionWithMessage() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("happened?");
+        thrown.expectMessage(startsWith("What"));
+        throw new NullPointerException("What happened?");
+    }
+}
--- src/main/java/play/JUnitCoreTest2.java
+++ src/main/java/play/JUnitCoreTest2.java
@@ -6,6 +6,14 @@ public class JUnitCoreTest2 {
     @Test
     public void shouldAddFailuresToResult() {
         System.out.println("hello world junit");
+
+        Object[] o1 = new Object[2];
+        o1[0] = "0";
+        o1[1] = "2";
+
+        Object[] o2 = o1;
+        o2[1] = "2";
+        int i =1;
     }
 
 
